b6c58b8645b01b9d47ed0681b56fd7c8171cbeea
Shidowy <101477459+Shidowy@users.noreply.github.com>
2024-10-28 08:22:48 -0700

    .

--- rafka/src/consumer.rs
+++ rafka/src/consumer.rs
@@ -2,7 +2,7 @@ use std::sync::{Arc, Mutex};
 use tokio::sync::mpsc;
 use super::broker::Broker; 
 
-// Update the function signature
+// Update the functionf signature
 pub async fn consume(broker: Arc<Mutex<Broker>>, topic: &str) {
     let mut receiver = broker.lock().unwrap().subscribe(topic.to_string()); // Lock the broker
     while let Some(message) = receiver.recv().await {
